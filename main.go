009899df871ffb48982ba531414c517782c30424
namkyu1999 <lak9348@konkuk.ac.kr>
2024-03-01 15:39:15 +0900

    feat: setup http handler
    Signed-off-by: namkyu1999 <lak9348@konkuk.ac.kr>

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/v1", v1)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func v1(w http.ResponseWriter, r *http.Request) {
+	response := "hello world"
+	w.Write([]byte(response))
 }
